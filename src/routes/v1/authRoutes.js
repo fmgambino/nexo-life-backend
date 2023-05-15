6a76c0b977344522416c0f4e7183563451317e55
Edgardo <chiuchiolo30@gmail.com>
2023-05-15 18:57:31 -0300

    fix errors express

--- src/routes/v1/authRoutes.js
+++ src/routes/v1/authRoutes.js
@@ -1,4 +1,6 @@
-import { Router } from "express";
+// import { Router } from "express";
+const { Router } = require('express');
+
 const router = Router();
 
 import authController from "../../controllers/authController.js";
