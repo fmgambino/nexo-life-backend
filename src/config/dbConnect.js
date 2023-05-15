3c82a2e3af024af607ba937caeed076ee96d60e4
Edgardo <chiuchiolo30@gmail.com>
2023-05-15 19:56:03 -0300

    fix mongoose connect

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,7 +3,16 @@ import mongoose from "mongoose";
 const dbConnect = () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(
+      process.env.MONGODB_URI, {
+        user: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false
+        }
+      }
+    );
     console.log("Database connected successfully!");
   } catch (error) {
     console.log("Database error!");
